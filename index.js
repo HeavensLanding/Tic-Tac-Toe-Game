4b70061ccbcf3998076cd2cf5a4685cdb33a26a0
HeavensLanding <rasmussencourtney0103@gmail.com>
2024-08-07 18:47:09 -0600

    const cells

---
+++ index.js
@@ -0,0 +1,14 @@
+const changeTurn = document.getElementById('changeTurn');
+const board = document.getElementById('board');
+const resetButton = document.getElementById('resetButton');
+
+/* List of cells */
+const cellOne = document.getElementById('1');
+const cellTwo = document.getElementById('2');
+const cellThree = document.getElementById('3');
+const cellFour = document.getElementById('4');
+const cellFive = document.getElementById('5');
+const cellSix = document.getElementById('6');
+const cellSeven = document.getElementById('7');
+const cellEight = document.getElementById('8');
+const cellNine = document.getElementById('9');

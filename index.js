be70e572c5f114a285e07b7eb6d1a6db47b29b5a
HeavensLanding <rasmussencourtney0103@gmail.com>
2024-08-14 16:39:21 -0600

    add

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+//have a section to display whose turns it
 const changeTurn = document.getElementById('changeTurn');
 const board = document.getElementById('board');
 const resetButton = document.getElementById('resetButton');
+
+//gameCells -s an array
+const gameCells = document.querySelectorAll('.game-cell');
+console.log("Game cells:", gameCells);
+
+
+
+gameCells.forEach(
+
+        (gameCell, index) => {
+    console.log(index,"Game Cell:", gameCell.id);
+
+    gameCell.addEventListener('click', () => {
+    console.log("Game cell has been clicked!!");
+
+    // gameCell.textContent = "x"
+    gameCell.innerHTML = "x"
+    })
+
+        }
+)
+
+
+
 
 /* List of cells */
 const cellOne = document.getElementById('1');
@@ -12,3 +37,11 @@ const cellSix = document.getElementById('6');
 const cellSeven = document.getElementById('7');
 const cellEight = document.getElementById('8');
 const cellNine = document.getElementById('9');
+
+
+
+//player x goes first
+//player o goes second
+
+
+//
